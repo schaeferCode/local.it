3df8b783ee10068ecab0c5218c775c12e4f5d1b7
Christine Zimmerman <ChristineOrtega@gmail.com>
2017-09-02 15:54:10 -0700

    Use a Discover and Saved component to make touchability work

---
+++ components/DiscoverTab.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NavigatorIOS, StyleSheet } from 'react-native';
+
+const DiscoverTab = ({props}) => (
+  <NavigatorIOS style={styles.container}/>
+)
+
+
+export default DiscoverTab;
---
+++ components/SavedTab.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { NavigatorIOS, StyleSheet } from 'react-native';
+
+const SavedTab = ({props}) => (
+  <NavigatorIOS style={styles.container}/>
+)
+
+export default SavedTab;
