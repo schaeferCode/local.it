78201da3596c382eacd50db1990d410377a9cc6d
Nick <nhattwick@gmail.com>
2017-09-02 22:35:18 -0700

    Prepping for rebase

---
+++ components/categories.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, View, TouchableOpacity, Image, Keyboard, TextInput, ScrollView, Linking } from 'react-native';
+import Item from 'item';
+export default class Categories extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: "Active Life",
+      item: "Suggestion"
+    }
+  }
+
+  render() {
+    return (
+      <View style = {{
+        flex: 2,
+        borderStyle: 'solid',
+        borderWidth: 5,
+        backgroundColor: '#fff',
+        marginTop: 50,
+      }}>
+        <Text>Back                               {this.state.category}</Text>
+        <Item>
+      </View>
+    );
+  }
+}
+
+
+
