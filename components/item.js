0a158145b89b72a73a9e486f68c8f72962e48774
Nick <nhattwick@gmail.com>
2017-09-04 08:16:37 -0700

    displaying example data for business

--- components/item.js
+++ components/item.js
@@ -8,10 +8,10 @@ export default class Item extends Component {
       data: {
         name: "Ichiza Kitchen & Tea House",
         image: "https://s3-media4.fl.yelpcdn.com/bphoto/EqRBNo8asZYltitUwQXtkQ/o.jpg",
-        Rating: 5,
-        Distance: "1.3 mi",
-        Description: "Vegan, Asian Fusion",
-        Money: "$$"
+        rating: 5,
+        distance: "1.3 mi",
+        description: "Vegan, Asian Fusion",
+        money: "$$"
       }
     }
   }
@@ -25,7 +25,15 @@ export default class Item extends Component {
           marginTop: 50
         }
       }>
-        <Image src = {this.state.data.image}/>
+        <Image
+        style={{width: 400, height: 300}}
+        source = {{ uri: this.state.data.image}}/>
+        <View>
+          <Text>{this.state.data.name}</Text>
+          <Text>{this.state.data.rating}</Text>
+          <Text>{this.state.data.distance}</Text>
+          <Text>{this.state.data.description}</Text>
+        </View>
       </View>
     )
   }
